c5c75b29a63ffe251815719d1a0edf17e3593b83
MIRACLEMUM <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-01-08 05:26:50 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numbers[3]);
+
+let students =['blessing', 'comfort' ,'happiness' ]
+
+let welcome = ['welcome!','everybody']
+
+console.log(welcome.join(' '))
+
+let flowers = ['rose','hibiscus','sunflowers']
+console.log(flowers[0])
+
+let names =['rose','blessing','miracle']
+console.log(names[2])
+
+let girls = ['miracle','uwana','happiness','faith']
+girls.push('favor');
+
+girls. unshift ('mimi');
+
+
+
