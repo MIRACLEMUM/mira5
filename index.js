d6bad8abd77e76d3095b06eea5c2f7cda6573308
MIRACLEMUM <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-01-18 01:48:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,49 @@ console.log(checkEvenOrOdd(11));
 console.log(checkEvenOrOdd(8));
 
 
+let colors = ['red','black','white','red','blue']
+console.log(colors);
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+
+
+console.log(colors.indexOf('red'))
+console.log(colors.includes('orange'))
+console.log(colors.includes('red'))
+
+
+colors [0] = 'yellow';
+console.log(colors)
+
+
+colors.push('pink')
+console.log(colors)
+
+colors.unshift('orange')
+console.log(colors)
+
+let sentence = ['my','love']
+
+console.log(sentence)
+
+console.log(sentence.join(' '))
+console.log(sentence.join('-'))
+
+let array = ['My','lord','help','me']
+console.log(array.join(' '))
+console.log(array.join('-'))
+
+
+
+
+
+
+
+
 
 
 
