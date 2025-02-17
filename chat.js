46b5e21a197b4dd6b921a574c6cf3ea75a92c52e
MIRACLEMUM <MIRACLE1FRIDAY2SUNDAY@GMAIL.COM>
2025-02-17 07:58:03 -0800

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,32 @@
+let num1 = 20
+let num2 = 10
+
+//arithmetic operations
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+console.log('sum:',sum)
+console.log('differnce:',difference)
+console.log('product:',product)
+console.log('quotient:',quotient)
+console.log('remainder:',remainder)
+
+/*
+//function
+function addNum(c,d) {
+    return c + d;
+}
+
+let result = addNum(50,70)
+console.log('sum',result)
+*/
+function subNum(e, f){
+    return e - f;
+}
+let re = subNum(100,39)
+console.log('difference',result)
+
+
